a738085cc7ada8913768a58b042734369dab3e95
renechoi <renechoi90@gmail.com>
2023-04-01 16:13:03 +0900

    feat: initial commit - project setup

---
+++ src/main/java/ssipgeukbbok/shoppingjpapractice/ShoppingJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package ssipgeukbbok.shoppingjpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingJpaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingJpaPracticeApplication.class, args);
+    }
+
+}
